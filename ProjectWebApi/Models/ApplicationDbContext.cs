9eaa77f8da766e252bf36b100ad039f51994dcce
Fatma Ahmed <ffaa4857@gmail.com>
2021-05-20 22:59:08 +0200

    Add project files.

---
+++ ProjectWebApi/Models/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ProjectWebApi.Models
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+
+        public ApplicationDbContext() : base("CS")
+        {
+
+        }
+        public virtual DbSet<Order> orders { get; set; }
+    }
+}
